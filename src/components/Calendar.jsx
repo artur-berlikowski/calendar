4b184c6dda23acd86a64df030bc50fe16e946537
artur-berlikowski <a.berlikowski@gmail.com>
2022-11-25 22:33:56 +0100

    Minimized code in Calendar component

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -15,26 +15,6 @@ import DatePicker from './DatePicker'
 const Calendar = (props) => {
   let { locale } = props
 
-  const [currentWeek, setCurrentWeek] = useState()
-  const [currentDate, setCurrentDate] = useState()
-  const [currentTime, setCurrentTime] = useState(new Date().toLocaleTimeString(locale, { hour: '2-digit', minute: '2-digit' }))
-  const [selection, setSelection] = useState({
-    year: new Date().getFullYear(),
-    month: new Date().getMonth(),
-    day: new Date().getDate()
-  })
-
-  useEffect(() => {
-    setCurrentWeek(getCurrentWeek())
-    setCurrentDate(getCurrentDate())
-
-    const time = setInterval(() => {
-      setCurrentTime(new Date().toLocaleTimeString(locale, { hour: '2-digit', minute: '2-digit' }))
-    }, 1000)
-
-    return () => clearInterval(time)
-  }, [])
-
   const getCurrentWeek = () => {
     let date = new Date()
     let start = new Date(date.getFullYear(), 0, 1)
@@ -43,19 +23,32 @@ const Calendar = (props) => {
     return Math.ceil(days / 7)
   }
 
-  const getCurrentDate = () => {
-    let options = { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' }
-    let date = new Date()
+  const [today, setToday] = useState({
+    year: new Date().getFullYear(),
+    month: new Date().getMonth(),
+    week: getCurrentWeek(),
+    day: new Date().getDate(),
+    time: new Date().toLocaleTimeString(locale, { hour: '2-digit', minute: '2-digit' }),
+    localeDateString: new Date().toLocaleDateString(locale, { weekday: 'long', day: 'numeric', month: 'long', year: 'numeric' })
+  })
 
-    return date.toLocaleDateString(locale, options)
-  }
+  useEffect(() => {
+    const time = setInterval(() => {
+      setToday({
+        ...today,
+        time: new Date().toLocaleTimeString(locale, { hour: '2-digit', minute: '2-digit' })
+      })
+    }, 1000)
+
+    return () => clearInterval(time)
+  }, [])
 
   return (
     <Container fluid id="calendar" className="d-flex flex-column justify-content-start align-items-start w-100 h-100 m-0 p-2">
       <Container fluid className="d-flex flex-row justify-content-between fw-bold g-0">
-        <div id="current_week">{currentWeek}</div>
-        <div id="today" className="text-capitalize">{currentDate}</div>
-        <div id="time">{currentTime}</div>
+        <div id="current_week">{today.week}</div>
+        <div id="today" className="text-capitalize">{today.localeDateString}</div>
+        <div id="time">{today.time}</div>
       </Container>
       <DatePicker {...{ locale }} />
     </Container>
