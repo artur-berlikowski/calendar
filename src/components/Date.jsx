74ec10bd9fde3fbe5c978d9edc55c99980f6ca84
artur-berlikowski <a.berlikowski@gmail.com>
2022-11-30 12:49:02 +0100

    Created skeleton for a Date component

--- src/components/Date.jsx
+++ src/components/Date.jsx
@@ -1,8 +1,13 @@
 const Date = (props) => {
-  let { year, month, day, week } = props
+  let { id, year, month, week, day, mute, highlight } = props
 
   return (
-    <></>
+    <td
+      id={id} className={`border-0 text-center align-middle ${mute ? 'text-muted' : ''}`}
+      style={{ height: '15,83%', background: `${highlight ? 'rgba(0,0,0,0.1)' : ''}`, cursor: 'pointer' }}
+    >
+      {day}
+    </td>
   )
 }
 
