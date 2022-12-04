5a97bf6c76f0732165472ee7f9445bd8b4f1a475
artur-berlikowski <a.berlikowski@gmail.com>
2022-12-04 01:50:10 +0100

    Cleaned up DatePicker component

--- src/components/DatePicker.jsx
+++ src/components/DatePicker.jsx
@@ -47,7 +47,7 @@ const DatePicker = (props) => {
   }
 
   const handleMonthBack = () => {
-    let { year, month, week, day } = selection
+    let { year, month, week } = selection
 
     if (year >= 1970) {
       let lastDayOfLastMonth = new Date(year, month, 0).getDate()
@@ -94,6 +94,8 @@ const DatePicker = (props) => {
 
   const generateDatesArray = async (year, month) => {
     let daysInCurrentMonth = new Date(month === 11 ? year + 1 : year, month === 11 ? 0 : month + 1, 0).getDate()
+    let lastMonth = month === 0 ? 11 : month - 1
+    let nextMonth = month === 11 ? 0 : month + 1
     let daysInLastMonth = new Date(year, month, 0).getDate()
     let startsOn = new Date(year, month, 0).getDay()
     let rows = []
@@ -101,12 +103,11 @@ const DatePicker = (props) => {
 
     for (let row = 0; row < 6; row++) {
       let notStarted = count < startsOn
-      let lastMonth = month === 0 ? 11 : month - 1
       let newRow = [{
-        year: selection.year,
-        month: selection.month,
+        year: year,
+        month: month,
         week: getWeekByDate(new Date(notStarted && lastMonth === 11 ? year - 1 : year, notStarted ? lastMonth : month, notStarted ? daysInLastMonth - (startsOn - 2) : count + 1)),
-        day: selection.day,
+        day: 1,
         isWeek: true,
         isLast: false,
         isNext: false,
@@ -114,32 +115,24 @@ const DatePicker = (props) => {
       }]
 
       for (let col = 0; col < 7; col++) {
-        let dayInNextMonth = count - (daysInCurrentMonth) - startsOn
+        let dayInNextMonth = count - daysInCurrentMonth - startsOn
         let isLast = count < startsOn
         let isNext = count > startsOn + daysInCurrentMonth - 1
-        let day = (isLast ? (daysInLastMonth - (startsOn - count)) : isNext ? dayInNextMonth : (count - startsOn)) + 1
+        let isCurrent = !isLast && !isNext
         let newYear = isLast && month === 0 ? year - 1 : isNext && month === 11 ? year + 1 : year
-        let newMonth = (month) => {
-          if (isLast) {
-            if (month === 0) return 11
-            else return month - 1
-          }
-          if (isNext) {
-            if (month === 11) return 0
-            else return month + 1
-          }
-          return month
-        }
+        let newMonth = isLast ? lastMonth : isNext ? nextMonth : month
+        let newDay = (isLast ? daysInLastMonth - (startsOn - count) : isNext ? dayInNextMonth : (count - startsOn)) + 1
+        let newWeek = getWeekByDate(new Date(newYear, newMonth, newDay))
+
         newRow.push({
           year: newYear,
-          month: newMonth(month),
-          week: getWeekByDate(new Date(year, month, day)),
-          day: day,
-          value: day + 1,
+          month: newMonth,
+          week: newWeek,
+          day: newDay,
           isWeek: false,
           isLast: isLast,
           isNext: isNext,
-          isCurrent: !isLast && !isNext
+          isCurrent: isCurrent
         })
         count++
       }
@@ -149,23 +142,23 @@ const DatePicker = (props) => {
   }
 
   const getWeekByDate = (date) => {
-    var dayNum = date.getUTCDay() || 7;
-    date.setUTCDate(date.getUTCDate() + 4 - dayNum);
+    var day = date.getUTCDay() || 7;
+    date.setUTCDate(date.getUTCDate() + 4 - day);
     var yearStart = new Date(Date.UTC(date.getUTCFullYear(), 0, 1));
     return Math.ceil((((date - yearStart) / 86400000) + 1) / 7)
   }
 
   return (
-    <Container fluid className="d-flex flex-column flex-fill justify-content-start align-items-center m-0 p-0">
-      <Container fluid className="d-flex flex-row justify-content-between align-items-center m-0 px-0 py-1">
-        <img src={arrowLeft} className="me-3" style={{ cursor: 'pointer' }} onClick={handleYearBack} />
+    <Container fluid className="d-flex flex-column flex-fill justify-content-start align-items-center g-0">
+      <Container fluid className="d-flex flex-row justify-content-between align-items-center g-0 py-1">
+        <img src={arrowLeft} style={{ cursor: 'pointer' }} onClick={handleYearBack} />
         <div id="selected_month" className="text-capitalize">{selection.year}</div>
-        <img src={arrowRight} className="ms-3" style={{ cursor: 'pointer' }} onClick={handleYearNext} />
+        <img src={arrowRight} style={{ cursor: 'pointer' }} onClick={handleYearNext} />
       </Container>
-      <Container fluid className="d-flex flex-row justify-content-between align-items-center m-0 px-0 py-1">
-        <img src={arrowLeft} className="me-3" style={{ cursor: 'pointer' }} onClick={handleMonthBack} />
+      <Container fluid className="d-flex flex-row justify-content-between align-items-center g-0 py-1">
+        <img src={arrowLeft} style={{ cursor: 'pointer' }} onClick={handleMonthBack} />
         <div id="selected_month" className="text-capitalize">{new Date(selection.year, selection.month, selection.day).toLocaleDateString(locale, { month: 'long' })}</div>
-        <img src={arrowRight} className="ms-3" style={{ cursor: 'pointer' }} onClick={handleMonthNext} />
+        <img src={arrowRight} style={{ cursor: 'pointer' }} onClick={handleMonthNext} />
       </Container>
       <Container className="d-flex flex-column justify-content-start align-items-center flex-fill">
         <Dates {...{ locale, selection, setSelection, data }} />
